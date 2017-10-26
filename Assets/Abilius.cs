a5093ab68a96b742216e39b31205a484e19b7929
pxai <p@pello.io>
2017-10-26 19:06:19 +0200

    Ok, jumping

--- Assets/Abilius.cs
+++ Assets/Abilius.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Abilius : MonoBehaviour {
+    private int speed = 8;
+    private const int STOPPED = 0;
+    private const int RUNNING = 1;
+    private const int JUMPING = 2;
+    private const int SHOOTING = 3;
 
-	// Use this for initialization
-	void Start () {
+    private int state = STOPPED;
+    private int previousState = STOPPED;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (state != JUMPING)
+            {
+                previousState = state;
+                this.GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+                state = JUMPING;
+            }
+        }
+        Debug.Log("Update " + state + "," + previousState);
+        this.transform.rotation = Quaternion.identity;
+    }
+
+    public void jumpEnded() {
+        state = previousState;
+        previousState = JUMPING;
+    }
 }
